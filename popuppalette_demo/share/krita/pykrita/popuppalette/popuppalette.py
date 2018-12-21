d79ea3bea8196821f0245c3d8c2ab58e95fd3131
Metallicow <metaliobovinus@gmail.com>
2018-12-21 12:25:33 -0600

    Address parents, Canvas Only Mode, post questionnaire aboutToQuit

--- popuppalette_demo/share/krita/pykrita/popuppalette/popuppalette.py
+++ popuppalette_demo/share/krita/pykrita/popuppalette/popuppalette.py
@@ -1,43 +1,110 @@
 """
-This is a simple example of a Python script for Krita.
-It demonstrates how to set up a custom popup!
+This is a example of a Python program/script/plugin for Krita.
+It demonstrates how to set up a custom popup frame that also works as a
+standalone program if needbe so developers can have a basic bridge for their
+artwork/data when working with external programs.
+
+
+This program/script/plugin is licensed CC 0 1.0, so that you can learn from it.
+
+------ CC 0 1.0 ---------------
+
+The person who associated a work with this deed has dedicated the work to the public domain by waiving all of his or her rights to the work worldwide under copyright law, including all related and neighboring rights, to the extent allowed by law.
+
+You can copy, modify, distribute and perform the work, even for commercial purposes, all without asking permission.
+
+https://creativecommons.org/publicdomain/zero/1.0/legalcode
 """
 
+#--PyQt5 Imports -- https://pypi.org/project/PyQt5/
+from PyQt5.QtCore import QTimer
 from PyQt5.QtGui import QCursor
 
+#--Krita Imports -- https://krita.org/
+import krita
 from krita import Krita, Extension
 
+#--Local Imports
 from . import popuppalette_demo
 
 
 class PopupPaletteExtension(Extension):
+    """"""
     def __init__(self, parent):
+        """"""
         super().__init__(parent)
 
+        self.kritaInstance = Krita.instance()
         self.actions = []
         self.popupframe = None
 
     def setup(self):
+        """"""
         pass
 
     def createActions(self, window):
+        """"""
         action = window.createAction("showpopuppalette", i18n("Popup Palette"))
-        action.setShortcut('Z')
+        # The action shortcut is defined in the 'popuppalette.action' file for
+        # the Extension class.
         action.triggered.connect(self.showhide)
         self.initialize()
 
     def initialize(self):
-        self.popupframe = popuppalette_demo.PopupPaletteFrame()
+        """"""
+        # Since this is the extension class we will want to parent our child
+        #  to the nun and sometimes trade it with the krita mainframe to maintain
+        #  'Always on Top' style flag when toggling the GL art canvas.
+        mainWindow = self.kritaInstance.activeWindow().qwindow()
+        self.popupframe = popuppalette_demo.PopupPaletteFrame(None)
+        self.popupframe.hide()
+
+        # When the krita mainframe close button is pressed,
+        #  the menu item "Quit" is triggered,
+        #  or the keyboard shortcut associated with "Quit" is pressed,
+        #  we will want to collect all our data before krita destroys itself(object).
+        #  In the standalone application this normally would call the closeEvent.
+        ##TODO: Need a way to save data before Yes/No/Cancel questionnaire as objects will be dead after the questionnaire.
+        krita.qApp.aboutToQuit.connect(self.saveData)  # This saves data after krita asks Yes/No/Cancel Modification questions.
+
+        self.kritaInstance.action('view_show_canvas_only').triggered.connect(self.onShowCanvasOnly)
 
     def showhide(self):
+        """"""
         if self.popupframe.isVisible():
+            self.popupframe.setParent(None)
             self.popupframe.hide()
         else:
+            # Center the frame on the cursor point.
             cursorPos = QCursor.pos()
-            self.popupframe.move(cursorPos.x() - 256, cursorPos.y() - 256)
+            textureWidth, textureHeight = 512, 512
+            cursorWidth, cursorHeight = 24, 24
+            self.popupframe.move(cursorPos.x() - textureWidth//2,
+                                 cursorPos.y() - textureHeight//2 - cursorHeight)
+            mainWindow = Krita().instance().activeWindow().qwindow()
+            self.popupframe.setParent(mainWindow)
             self.popupframe.show()
             self.popupframe.activateWindow()
 
+    def saveData(self):
+        """"""
+        self.popupframe.saveData()
 
-# Initialize and add the extension
+    def onShowCanvasOnly(self):
+        """"""
+        QTimer.singleShot(1, self.reparentPopup)  # Call After
+
+    def reparentPopup(self):
+        """"""
+        # Need to keep the popup from getting covered up by the GLCanvas when
+        # switching back and forth from canvas only view.
+        if self.popupframe.isVisible():
+            self.popupframe.setParent(None)
+            self.popupframe.hide()
+            self.popupframe.setParent(Krita().instance().activeWindow().qwindow())
+            self.popupframe.show()
+            self.popupframe.setFocus()
+
+
+# Initialize and add the extension.
 Scripter.addExtension(PopupPaletteExtension(Krita.instance()))
--- popuppalette_demo/share/krita/pykrita/popuppalette/popuppalette_demo.py
+++ popuppalette_demo/share/krita/pykrita/popuppalette/popuppalette_demo.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#--Python Imports.
+"""
+This is a example of a Python program/script/plugin for Krita.
+It demonstrates how to set up a custom popup frame that also works as a
+standalone program if needbe so developers can have a basic bridge for their
+artwork/data when working with external programs.
+THIS file is the standalone program.
+
+
+This program/script/plugin is licensed CC 0 1.0, so that you can learn from it.
+
+------ CC 0 1.0 ---------------
+
+The person who associated a work with this deed has dedicated the work to the public domain by waiving all of his or her rights to the work worldwide under copyright law, including all related and neighboring rights, to the extent allowed by law.
+
+You can copy, modify, distribute and perform the work, even for commercial purposes, all without asking permission.
+
+https://creativecommons.org/publicdomain/zero/1.0/legalcode
+"""
+
+#--Python Imports. https://www.python.org/
 import os
 import sys
 ## import datetime
@@ -11,19 +30,17 @@ try:
 except:
     gFileDir = os.path.dirname(os.path.abspath(sys.argv[0]))
 gImgDir = gFileDir + os.sep + 'images'
-
 gPyKritaDir = os.path.dirname(gFileDir)
 gKritaDir = os.path.dirname(gPyKritaDir)
 gPicsDir = gKritaDir + os.sep + 'pics'
 gShareDir = os.path.dirname(gKritaDir)
 
-
-#--PySide/PyQt Imports.
+#--PySide/PyQt Imports. https://pypi.org/project/PyQt5/
 from PyQt5.QtGui import  QBitmap, QBrush, QColor, QCursor, QImage, QLinearGradient, QPainter, QPen, QPixmap
-from PyQt5.QtCore import Qt, QSize, QPoint, QRect
-from PyQt5.QtWidgets import QAction, QApplication, QAbstractButton, QDialog, QListWidget, QListWidgetItem, QMessageBox, QWidget
+from PyQt5.QtCore import Qt, QEvent, QPoint, QRect, QSize
+from PyQt5.QtWidgets import QAbstractButton, QAction, QApplication, QDialog, QMenu, QMessageBox, QWidget, QTextEdit
 
-
+#--Krita Imports -- https://krita.org/
 try:
     import krita
 except ImportError:
@@ -57,7 +74,7 @@ class ImageButton(QAbstractButton):
     def enterEvent(self, event):
         self.update()
         # QApplication.setOverrideCursor(QCursor(Qt.ArrowCursor))
-        QApplication.setOverrideCursor(QCursor(QPixmap(gImgDir + os.sep + 'paperairplane_arrow_white24.png'), hotX=1, hotY=1))
+        QApplication.setOverrideCursor(QCursor(QPixmap(gImgDir + os.sep + 'paperairplane_arrow_white24.png'), hotX=0, hotY=0))
 
     def leaveEvent(self, event):
         self.update()
@@ -74,13 +91,15 @@ class PopupPaletteFrame(QDialog):
         self.setAttribute(Qt.WA_NoSystemBackground, True)
         self.setAttribute(Qt.WA_TranslucentBackground, True)
 
+        self.setGeometry(0, 0, 512, 512)
+
         self.offset = QPoint(0, 0)
 
         self.local_image = QImage(512, 512, QImage.Format_ARGB32)
         self.fgcolor = '#FFFFFF'
         self.bgcolor = '#000000'
 
-        # This is how to load the merged image from a .kra file
+        # This is how to load the merged image from a .kra file.
         ## import zipfile
         ## archive = zipfile.ZipFile(gImgDir + os.sep + 'myPopupPaletteWorkshop.kra', 'r')
         ## img_data = archive.read('mergedimage.png')
@@ -95,14 +114,28 @@ class PopupPaletteFrame(QDialog):
 
         lHiColorDir = gShareDir + os.sep + 'icons' + os.sep + 'hicolor'
 
-        # Big Button in middle
+        # Text box.
+        self.textbox = QTextEdit("Test QTextEdit", parent=self)
+        self.textbox.move(192, 160)
+        self.textbox.setGeometry(192, 160, 128, 32)
+
+        # Big Button in middle.
         self.btnImage = btnImage = QImage(lHiColorDir + os.sep + '128x128' + os.sep + 'apps' + os.sep + 'calligrakrita.png')
         self.button = ImageButton(btnImage, btnImage, btnImage, self)
         self.button.move(256 - 64, 256 - 64)
+        self.button.pressed.connect(self.buttonPressed)
         self.button.released.connect(self.buttonReleased)
         self.button.setToolTip('Right Click for Context Menu')
+        self.button.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.button.customContextMenuRequested.connect(self.show_context_menu)
 
-        # 4 smaller buttons
+        # Create popup context menu.
+        self._context_menu = QMenu(self)
+        self._context_menu.addAction('Invert', self.invertTexture)
+        self._context_menu.addAction('Mirror Horizontal', self.mirrorTextureHorizontal)
+        self._context_menu.addAction('Mirror Vertical', self.mirrorTextureVertical)
+
+        # 4 smaller buttons.
         self.btnImage1 = btnImage1 = QImage(lHiColorDir + os.sep + '32x32' + os.sep + 'apps' + os.sep + 'calligrakrita.png')
         self.button1 = ImageButton(btnImage1, btnImage1, btnImage1, self)
         self.button1.move(192, 320)
@@ -136,10 +169,11 @@ class PopupPaletteFrame(QDialog):
         self.defblackwhiteBtn.move(436, 486)
         self.defblackwhiteBtn.pressed.connect(self.defaultFG_BG_Colors)
 
-        quitAction = QAction("E&xit", self, shortcut="Z",
-                             triggered=self.close)
-        self.button.addAction(quitAction)
-        self.button.setContextMenuPolicy(Qt.ActionsContextMenu)
+        if not krita:
+            quitAction = QAction("E&xit", self, shortcut="Z",
+                                 triggered=self.close)
+            self.button.addAction(quitAction)
+            self.button.setContextMenuPolicy(Qt.ActionsContextMenu)
 
         # Mask - Anything not black (0, 0, 0, 0-255) in this image will not be shown when drawn out.
         self.setMask(QBitmap(gImgDir + os.sep + "pixel_palette_mask512.png"))
@@ -148,8 +182,25 @@ class PopupPaletteFrame(QDialog):
         self.setFocusPolicy(Qt.StrongFocus)
         self.setFocus()
 
+        self.setMouseTracking(True)
+
     def __del__(self):
         print('__del__ %s' % self.__class__.__name__)
+
+    def mirrorTextureHorizontal(self):
+        self.texture = self.texture.mirrored(horizontal=True, vertical=False)
+        self.drawGUI()
+        self.update()
+
+    def mirrorTextureVertical(self):
+        self.texture = self.texture.mirrored(horizontal=False, vertical=True)
+        self.drawGUI()
+        self.update()
+
+    def invertTexture(self):
+        self.texture.invertPixels()
+        self.drawGUI()
+        self.update()
 
     def swapFG_BG_Colors(self):
         self.fgcolor, self.bgcolor = self.bgcolor, self.fgcolor
@@ -162,11 +213,18 @@ class PopupPaletteFrame(QDialog):
         self.drawGUI()
         self.update()
 
+    def show_context_menu(self, point):
+        self._context_menu.exec_(self.button.mapToGlobal(point))
+
+    def buttonPressed(self):
+        pass
+
     def buttonReleased(self):
         QApplication.beep()
 
     def button1Released(self):
-        QMessageBox.information(QWidget(self), i18n("Test Btn1"), i18n("Hello! This is Krita version %s") % Application.version())
+        QMessageBox.information(QWidget(self), i18n("Test Btn1"),
+                                i18n("Hello! This is Krita version %s\n\nwidth %s height %s") % (Application.version(), self.width(), self.height()))
 
     def button2Released(self):
         krita.Krita.instance().action('rotate_canvas_left').trigger()
@@ -175,9 +233,15 @@ class PopupPaletteFrame(QDialog):
         krita.Krita.instance().action('rotate_canvas_right').trigger()
 
     def button4Released(self):
-        QMessageBox.information(QWidget(self), i18n("Test Btn4"), i18n('"The position of the artist is humble. He is essentially a channel." Piet Mondrian'))
+        QMessageBox.information(QWidget(self), i18n("Test Btn4"),
+                                i18n('"The position of the artist is humble. He is essentially a channel." Piet Mondrian'))
+
+    def saveData(self):
+        with open(gFileDir + os.sep + 'test.txt', 'w') as f:
+            f.write('I Closed Cleanly and wrote out some data')
 
     def closeEvent(self, event):
+        self.saveData()
         event.accept()
 
     def sizeHint(self):
@@ -258,7 +322,6 @@ class PopupPaletteFrame(QDialog):
     def mousePressEvent(self, event):
         if event.button() == Qt.MiddleButton:
             self.offset = event.globalPos() - self.frameGeometry().topLeft()
-            # event.accept()
             ## print(self.offset)
         elif event.button() == Qt.LeftButton:
             position = QPoint(event.pos().x(), event.pos().y())
@@ -289,15 +352,27 @@ class PopupPaletteFrame(QDialog):
         if event.buttons() == Qt.MiddleButton:
             self.move(event.globalPos() - self.offset)
             event.accept()
+        self.textbox.setPlainText('x = %s, y = %s' % (event.x(), event.y()))
+
+    ## def wheelEvent(self, event):
+    ##     event.accept()
 
     def enterEvent(self, event):
         self.update()
-        # QApplication.setOverrideCursor(QCursor(Qt.ArrowCursor))
-        QApplication.setOverrideCursor(QCursor(QPixmap(gImgDir + os.sep + 'paperairplane_arrow_white24.png'), hotX=1, hotY=1))
+        ## QApplication.setOverrideCursor(QCursor(Qt.ArrowCursor))
+        QApplication.setOverrideCursor(QCursor(QPixmap(gImgDir + os.sep + 'paperairplane_arrow_white24.png'), hotX=0, hotY=0))
 
     def leaveEvent(self, event):
         self.update()
         QApplication.restoreOverrideCursor()
+
+    def event(self, event):
+        if event.type() == QEvent.KeyPress and event.key() == Qt.Key_Tab:
+            ## print("Tab pressed")
+            if krita:
+                krita.Krita.instance().action('view_show_canvas_only').trigger()
+            return False
+        return QWidget.event(self, event)
 
 
 def main():
